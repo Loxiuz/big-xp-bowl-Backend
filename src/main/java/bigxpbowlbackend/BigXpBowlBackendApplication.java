9fecd4884911dc7150d9e56aab721b93063b0a3a
Malte <91071750+Loxiuz@users.noreply.github.com>
2024-05-08 15:15:37 +0200

    Initial commit

---
+++ src/main/java/bigxpbowlbackend/BigXpBowlBackendApplication.java
@@ -0,0 +1,13 @@
+package bigxpbowlbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigXpBowlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigXpBowlBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bigxpbowlbackend/BigXpBowlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bigxpbowlbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigXpBowlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
